075df9e317b261898972d7674bb2c118611e5e33
Tim McCormack <tmccormack@edx.org>
2020-06-29 18:35:54 +0000

    Use requirements file when advertising dependencies in setup.py
    Also:
    
    - Fix PHONY targets in Makefile
    - Use pip-sync when installing requirements

--- setup.py
+++ setup.py
@@ -8,6 +8,30 @@ from setuptools import setup, find_packages
 from os import path
 from io import open
 
+def load_requirements(*requirements_paths):
+    """
+    Load all requirements from the specified requirements files.
+    Returns:
+        list: Requirements file relative path strings
+    """
+    requirements = set()
+    for path in requirements_paths:
+        requirements.update(
+            line.split('#')[0].strip() for line in open(path).readlines()
+            if is_requirement(line.strip())
+        )
+    return list(requirements)
+
+
+def is_requirement(line):
+    """
+    Return True if the requirement line is a package requirement.
+    Returns:
+        bool: True if the line is not blank, a comment, a URL, or
+              an included file
+    """
+    return line and not line.startswith(('-r', '#', '-e', 'git+', '-c'))
+
 here = path.abspath(path.dirname(__file__))
 
 with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
@@ -19,11 +43,7 @@ setup(
     packages=find_packages(exclude=['contrib', 'docs', 'tests']),  # Required
     python_requires=">=3.6",
     include_package_data=True,
-    install_requires=[
-    	'pyyaml',
-        'cryptography',
-        'click',
-    ],
+    install_requires=load_requirements('requirements/requirements.in'),
     entry_points='''
         [console_scripts]
         asym_crypto_yaml=scripts.asym_crypto_yaml:cli
