a64c196c9fe4b42ae3db6be6efd6807d57aee623
Zulqarnain <muhammad.zulqarnain@arbisoft.com>
2021-01-14 17:34:57 +0500

    Drop Python 3.6

--- setup.py
+++ setup.py
@@ -38,9 +38,9 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='asym_crypto_yaml',  # Required
-    version='0.0.16',  # Required
+    version='0.0.20',  # Required
     packages=find_packages(exclude=['contrib', 'docs', 'tests']),  # Required
-    python_requires=">=3.6",
+    python_requires=">=3.8",
     include_package_data=True,
     install_requires=load_requirements('requirements/base.in'),
     entry_points='''
